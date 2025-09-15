aaf544ac3d6371c5c20f2e47e0440109d02c19e9
mohamedalaaaz <88798693+mohamedalaaaz@users.noreply.github.com>
2025-09-15 18:34:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import  socket
+
+sock =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket.setdefaulttimeout(2)
+port=int(input("[*] enter port to scan"))
+host =input("[*]enter host to scan:!]")
+#198.164.56.1
+
+def portscanne (port):
+    if sock.connect_ex((host,port)):
+        print("port is close %d " %(port))
+
+    else:
+        print("port is open %d" % (port))
+
+for port in range(1,100):
+
+    portscanne(port)
---
+++ portscanner44.py
@@ -0,0 +1,19 @@
+import  socket
+from termcolor import colored
+
+sock =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#socket.setdefaulttimeout(2)
+
+host =input("[*]enter host to scan:!]")
+#198.164.56.1
+
+def portscanne (port):
+    if sock.connect_ex((host,port)):
+        print(colored("port is close %d " %(port),"red"))
+
+    else:
+        print(colored("port is open %d" % (port),"green"))
+
+for port in range(1,1000):
+
+    portscanne(port)
