1a63d5c01a94eb500c8bf96376e290d05ff3964f
mohamedalaaaz <88798693+mohamedalaaaz@users.noreply.github.com>
2025-09-16 18:40:46 +0300

    Add files via upload

---
+++ portscanner3.py
@@ -0,0 +1,37 @@
+from socket import *
+from termcolor import colored
+import optparse
+from threading import *
+
+def portscan(tgthost,tgtport):
+    try:
+        tgtip=gethostbyname(tgthost)
+    except:
+        print('unknown host s%' %tgthost)
+
+    try:
+        tgtname=gethostbyaddr(tgtip)
+        print(tgtname[0])
+
+    except:
+        print("scan results"+tgtip)
+
+    setdefaulttimeout(1)
+    for tgtport in tgtport:
+        t=Thread(traget=connscan,args=(tgthost),int(tgtport)))
+        t.start()
+
+
+def main():
+    parser =optparse.OptionParser('usage of program: '+'-H <target host > -p <target port>')
+    parser.add_option('-H',dest='tgthost',type='string',help='specify target host')
+    parser.add_option('-p', dest='tgtport', type='string', help='specify target ports seperated by coma ')
+    (options,args)=parser.parse_args()
+    tgthost=options.tgthost
+    tgtport=str(options.tgtport).split(',')
+    if ((tgthost == None )| (tgtport[0] == None)):
+        print(parser.usage)
+        exit(0)
+
+if __name__ == '__main__':
+    main()
